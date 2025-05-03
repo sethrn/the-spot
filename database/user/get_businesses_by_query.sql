34a54e5f521413b0c7fe9c0ee86e6b4a2cb4b330
sethrn <123341987+sethrn@users.noreply.github.com>
2025-05-02 21:13:09 -0700

    Adding search

--- database/user/get_businesses_by_query.sql
+++
@@ -1,8 +0,0 @@
-SELECT business_id, name, stars, review_count, categories
-FROM business
-WHERE
-    (%s IS NULL OR name ILIKE '%' || %s || '%')
-    AND (%s IS NULL OR state = %s)
-    AND (%s IS NULL OR is_open = %s::boolean)
-ORDER BY stars DESC
-LIMIT %s OFFSET %s;
